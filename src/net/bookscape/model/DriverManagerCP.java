3a19063a778b8f6bb6c6e5dfe4997b38405b1e1f
mickcpp <marco.renella03@gmail.com>
2024-07-25 18:46:24 +0200

    modificato metodo createDBConnection

--- src/net/bookscape/model/DriverManagerCP.java
+++ src/net/bookscape/model/DriverManagerCP.java
@@ -2,7 +2,9 @@ package net.bookscape.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,17 +22,43 @@ public class DriverManagerCP {
 	}
 	
 	private static synchronized Connection createDBConnection() throws SQLException {
-		Connection newConnection = null;
-		String ip = "localhost";
-		String port = "3306";
-		String db = "bookscape";
-		String username = "root";
-		String password = "password";
+	    Connection newConnection = null;
+	    String ip = "localhost";
+	    String port = "3306";
+	    String db = "bookscape";
+	    String username = "root";
+	    String password = "password";
+	    String truststorePath = "/opt/jdk-21.0.4/lib/security/cacerts";
+	    String truststorePassword = "changeit";
 
-		newConnection = DriverManager.getConnection("jdbc:mysql://"+ ip+":"+ port+"/"+db+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Rome", username, password);
+	    // Imposta le proprietà di sistema per il truststore
+	    System.setProperty("javax.net.ssl.trustStore", truststorePath);
+	    System.setProperty("javax.net.ssl.trustStorePassword", truststorePassword);
 
-		newConnection.setAutoCommit(true);
-		return newConnection;
+	    newConnection = DriverManager.getConnection(
+	        "jdbc:mysql://" + ip + ":" + port + "/" + db +
+	        "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Rome&useSSL=true&verifyServerCertificate=true&enabledTLSProtocols=TLSv1.2&requireSSL=true",
+	        username, password
+	    );
+
+	    newConnection.setAutoCommit(true);
+
+	    // Verifica se SSL è stato utilizzato
+	    try (Statement stmt = newConnection.createStatement();
+		        ResultSet rs = stmt.executeQuery("SHOW SESSION STATUS LIKE 'Ssl_cipher'")){
+	        if (rs.next()) {
+	            String sslCipher = rs.getString("Value");
+	            if (sslCipher != null && !sslCipher.isEmpty()) {
+	                System.out.println("SSL è attivo. Cifrario SSL utilizzato: " + sslCipher);
+	            } else {
+	                System.out.println("SSL non è attivo.");
+	            }
+	        }
+	    } catch (SQLException e) {
+	        System.err.println("Errore durante la verifica dell'SSL: " + e.getMessage());
+	    }
+
+	    return newConnection;
 	}
 
 
