2863229bdcb38a0a72b817f7e52eb674b5d9a8b3
mickcpp <marco.renella03@gmail.com>
2024-07-29 21:16:52 +0200

    aggiornamento a jakarta

--- src/net/bookscape/control/GadgetCatalogSearch.java
+++ src/net/bookscape/control/GadgetCatalogSearch.java
@@ -6,12 +6,12 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import net.bookscape.model.Gadget;
 import net.bookscape.model.ProductModelDM;
--- src/net/bookscape/control/ProductSearchAdmin.java
+++ src/net/bookscape/control/ProductSearchAdmin.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import net.bookscape.model.CsrfTokens;
 import net.bookscape.model.Product;
